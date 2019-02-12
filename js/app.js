d964df9e98f1ee4b55f13a696f40e7f60eb3e67e
tytbeo <46708223+tytbeo@users.noreply.github.com>
2019-02-12 20:18:26 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,18 +3,66 @@ var app = angular.module('myApp',['ngRoute']);
 app.config(function($routeProvider) {
   $routeProvider
   .when("/", {
-    templateUrl : "html/home.html"
+    templateUrl : "html/home.html",
+    controller : "homeCtrl"
   })
   .when("/gallery", {
-    templateUrl : "html/gallery.html"
+    templateUrl : "html/gallery.html",
+    controller : "galleryCtrl"
   })
   .when("/products", {
-    templateUrl : "html/products.html"
+    templateUrl : "html/products.html",
+    controller : "productsCtrl"
   })
   .when("/aboutus", {
-    templateUrl : "html/aboutus.html"
+    templateUrl : "html/aboutus.html",
+    controller : "aboutusCtrl"
   })
   .when("/contactus", {
-    templateUrl : "html/contactus.html"
+    templateUrl : "html/contactus.html",
+    controller : "contactusCtrl"
+  });
+});
+
+app.controller('homeCtrl', function($scope){
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 5,
+    dots: false,
+    nav: true,
+    navText: [
+      "<i class='fa fa-chevron-left'></i>",
+      "<i class='fa fa-chevron-right'></i>"
+    ],
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 4
+      }
+    }
+  });
+
+});
+app.controller('galleryCtrl', function($scope){
+  $(".filter-button").on('click' ,function() {
+    var value = $(this).attr('data-filter');
+    $(".filter-button").removeClass("actives");
+    $(this).addClass("actives");
+    if (value == "all") {
+      //$('.filter').removeClass('hidden');
+      $('.filter').show('1000');
+    } else {
+      //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+      //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+      $(".filter").not('.' + value).hide('slow');
+      $('.filter').filter('.' + value).show('slow');
+
+    }
   });
 });
--- js/main.js
+++ js/main.js
@@ -11,46 +11,8 @@ function collapseChat() {
   document.getElementById("chat-text").style.display = 'none';
 };
 
- $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 5,
-    dots: false,
-    nav: true,
-    navText: [
-      "<i class='fa fa-chevron-left'></i>",
-      "<i class='fa fa-chevron-right'></i>"
-    ],
-    autoplay: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 1
-      },
-      1000: {
-        items: 4
-      }
-    }
-  });
-  $(".filter-button").on('click' ,function() {
-    var value = $(this).attr('data-filter');
-    $(".filter-button").removeClass("actives");
-    $(this).addClass("actives");
-    if (value == "all") {
-      //$('.filter').removeClass('hidden');
-      $('.filter').show('1000');
-    } else {
-      //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-      //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-      $(".filter").not('.' + value).hide('slow');
-      $('.filter').filter('.' + value).show('slow');
-
-    }
-  });
 
 $(document).ready(function() {
- 
   $('#chat-input').on('keypress', function(e) {
     // e.preventDefault();
 
