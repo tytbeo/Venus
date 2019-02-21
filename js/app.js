c63dcf44fd03eb5e6ed41f2fa1c2ddb6521199e1
Giang <g.hnguyen@yahoo.com>
2019-02-21 19:23:12 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -404,4 +404,76 @@ app.controller('detailsCtrl',['$scope', '$stateParams','$cookies',function($scop
   $scope.moreText = "Read more";
   $scope.dotsClass = "toggle-dots-grey";
   $scope.linkClass = "toggle-link-yellow";
+
+  // view cart
+  $scope.cart = [];
+  $scope.total = 0;
+
+  if (!angular.isUndefined($cookies.get('total'))) {
+    $scope.total = parseFloat($cookies.get('total'));
+  }
+  //Sepetimiz daha önceden tanımlıysa onu çekelim
+  if (!angular.isUndefined($cookies.get('cart'))) {
+    $scope.cart = $cookies.getObject('cart');
+  }
+
+  $scope.addItemToCart = function(product) {
+
+    if ($scope.cart.length === 0) {
+      product.count = 1;
+      $scope.cart.push(product);
+    } else {
+      var repeat = false;
+      for (var i = 0; i < $scope.cart.length; i++) {
+        if ($scope.cart[i].id === product.id) {
+          repeat = true;
+          $scope.cart[i].count += 1;
+        }
+      }
+      if (!repeat) {
+        product.count = 1;
+        $scope.cart.push(product);
+      }
+    }
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() + 1);
+    $cookies.putObject('cart', $scope.cart, {
+      'expires': expireDate
+    });
+    $scope.cart = $cookies.getObject('cart');
+
+    $scope.total += parseFloat(product.price);
+    $cookies.put('total', $scope.total, {
+      'expires': expireDate
+    });
+  };
+
+  $scope.removeItemCart = function(product) {
+
+    if (product.count > 1) {
+      product.count -= 1;
+      var expireDate = new Date();
+      expireDate.setDate(expireDate.getDate() + 1);
+      $cookies.putObject('cart', $scope.cart, {
+        'expires': expireDate
+      });
+      $scope.cart = $cookies.getObject('cart');
+    } else if (product.count === 1) {
+      var index = $scope.cart.indexOf(product);
+      $scope.cart.splice(index, 1);
+      expireDate = new Date();
+      expireDate.setDate(expireDate.getDate() + 1);
+      $cookies.putObject('cart', $scope.cart, {
+        'expires': expireDate
+      });
+      $scope.cart = $cookies.getObject('cart');
+
+    }
+
+    $scope.total -= parseFloat(product.price);
+    $cookies.put('total', $scope.total, {
+      'expires': expireDate
+    });
+
+  };
 }]);
